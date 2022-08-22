945d36bfeb4ddc3b8424aef66824919d3643f4b3
Mile-pixel <52139433+Mile-pixel@users.noreply.github.com>
2022-08-22 00:14:12 -0300

    ejer3nvo

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,24 +1,49 @@
-function binario(arreglo,nro){
-    let i= 0;
-    let j= arreglo.length;
-    let tope=false;
+function SelectionSort(arreglo) {
+  for (let a = 0; a < arreglo.length; a++) {
+      for (let j = a + 1; j < arreglo.length; j++) {
+          if (arreglo[j] < arreglo[a]) {
+              let aux = arreglo[a];
+              arreglo[a] = arreglo[j];
+              arreglo[j] = aux;
+          }
+          else if (arreglo[j] > arreglo[a]) {
 
-    while (tope == false) {
-        let centro = Math.floor((i+j)/2);
-        if ( centro == nro ){
-          tope= true;
-          
-    }
-        else if ( nro> centro ) {
-          i = centro;
-        } else if(nro< centro) {
-            j = centro+1;
-        }
-    }
-    if (tope==true){
-      return true;
-    }
-       }
-   
+          }
 
-console.log(binario([1,2,3,4,5,6,7,8,9],9));
+      }
+
+  }
+  return arreglo;
+}
+
+
+let ordenado = SelectionSort([1, 0, 5, 2, 3,7,4,6,9]);
+
+
+
+
+function binario(ordenado,nro){
+  let i= 0;
+  let j= ordenado.length +1;
+  let tope=false;
+
+  while (tope == false) {
+      let centro = Math.floor((i+j)/2);
+      if ( centro == nro ){
+        tope= true;
+
+  }
+  else if ( nro> centro ) {
+    i = centro;
+  } else if(nro< centro) {
+      j = centro;
+  }
+
+}
+if (tope==true){
+return true;
+}
+
+}
+
+console.log(binario(ordenado,9)); 
